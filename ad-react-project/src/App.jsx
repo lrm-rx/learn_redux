fc49fcf53f13a1e16d99a8869b2f8d5f55b065a8
lrm <756131502@qq.com>
2023-01-04 19:58:58 +0800

    初始化项目

---
+++ ad-react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+import './style.scss'
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="title">
+        Hello, React!
+      </div>
+    )
+  }
+}
+
+export default App
